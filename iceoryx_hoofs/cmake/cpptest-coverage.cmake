5dab9fda762915b0f6081b9638fec39faff1773d
mzwawa23 <mzwawa@parasoft.com>
2025-04-23 10:04:38 +0200

    updated cpptest_coverage.cmake and main.yml

--- iceoryx_hoofs/cmake/cpptest-coverage.cmake
+++ iceoryx_hoofs/cmake/cpptest-coverage.cmake
@@ -24,11 +24,11 @@ function (cpptest_enable_coverage)
   endif() 
 
   # Configure C/C++test compiler identifier
-  set(CPPTEST_COMPILER_ID "gcc_11-64")
+  set(CPPTEST_COMPILER_ID "gcc_10-64")
   # Configure coverage type(s) for instrumentation engine - see 'cpptestcc -help' for details
   set(CPPTEST_COVERAGE_TYPE_INSTRUMENTATION -template-coverage -constexpr-coverage -line-coverage -statement-coverage -block-coverage -decision-coverage -simple-condition-coverage -mcdc-coverage -function-coverage -call-coverage)
   # Configure coverage type(s) for reporting engine - see 'cpptestcov -help' for details
-  set(CPPTEST_COVERAGE_TYPE_REPORT "LC, SC, DC,MCDC" )
+  set(CPPTEST_COVERAGE_TYPE_REPORT "LC,SC,BC,DC,CC,MCDC" )
   # Configure C/C++test project name
   set(CPPTEST_PROJECT_NAME ${CMAKE_PROJECT_NAME})
   # Configure coverage workspace folder
@@ -97,7 +97,7 @@ function (cpptest_enable_coverage)
       -exclude "regex:${CPPTEST_BINARY_DIR}/*"
       -ignore "regex:${CPPTEST_BINARY_DIR}/*"
   #    -psrc "${CPPTEST_SOURCE_DIR}/cmake/.cpptestcc")
-  )
+    )
 
   # Use advanced settings file for cpptestcc, if exists
   if(EXISTS "${CMAKE_SOURCE_DIR}/.cpptestcc")
